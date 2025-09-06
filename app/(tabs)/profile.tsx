f889e0c6d942d93e1757f7ada789b0895538d81d
GlendyT <glendytuyuc@gmail.com>
2025-09-05 18:35:13 -0600

    finishing the course, however I will start to create a profile and how to save favorite movies

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { icons } from "@/constants/icons";
+import React from "react";
+import { Image, Text, View } from "react-native";
 
 const profile = () => {
   return (
-    <View>
-      <Text>profile</Text>
+    <View className="bg-primary flex-1 px-10 ">
+      <View className="flex justify-center items-center flex-1 flex-col gap-2">
+        <Image source={icons.person} className="size-10" tintColor="#fff" />
+        <Text>profile</Text>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default profile
-
+export default profile;
